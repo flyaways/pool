d50cc74067f9a9879eb3a0597cd97f7f3f345fec
flyaways <meagleglass@gmail.com>
2018-08-30 20:26:38 +0800

    更新例子

--- _example/main.go
+++ _example/main.go
@@ -5,43 +5,56 @@ import (
 	"time"
 
 	"github.com/flyaways/pool"
+	"google.golang.org/grpc"
 )
 
-func main() {
-	p, err := pool.New(
-		"tcp",
-		"127.0.0.1:8080",
-		5,
-		30,
-		time.Second*5, //dialTimeout
-		time.Second*5, //idleTimeout
-	)
+func main() { 
+	//初始化创建连接池
+	p, err := pool.NewChannelPool(&PoolConfig{
+		InitialCap:  5,
+		MaxCap:      30,
+		Factory:     pool.FactoryGRPC,
+		Close:       pool.CloseGRPC,
+		IdleTimeout: time.Second*5,
+		DialTimeout:time.Second*5,
+		 DialOptions:[]grpc.DialOption{
+         	grpc.WithInsecure()
+		 },
+	})
 
+	//异常处理
 	if err != nil {
-		fmt.Printf("%#v\n", err)
+		log.Printf("%#v\n", err)
 		return
 	}
 
+	//异常处理
 	if p == nil {
-		fmt.Printf("p= %#v\n", p)
+		log.Printf("p= %#v\n", p)
 		return
 	}
 
+	//释放连接池
 	defer p.Release()
 
+	//获取一个连接
 	v, err := p.Get()
 	if err != nil {
-		fmt.Printf("%#v\n", err)
+		log.Printf("%#v\n", err)
 		return
 	}
 
-	//todo
-	//conn=v.(net.Conn)
+	//todo 执行业务逻辑
+	//conn=v.(*grpc.ClientConn)
 
-	if p.Put(v) != nil {
-		fmt.Printf("%#v\n", err)
-		return
-	}
+	//放回连接池
+	defer func() {
+		if p.Put(v) != nil {
+			log.Printf("%#v\n", err)
+			return
+		}
+	}()
 
-	fmt.Printf("len=%d\n", p.Len())
+	//打印连接池大小
+	log.Printf("len=%d\n", p.Len())
 }

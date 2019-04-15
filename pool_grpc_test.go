545a127a18f0e9064e9ebda05b5bbe2c8e1b9b38
flyaways <meagleglass@gmail.com>
2019-04-15 19:14:42 +0800

    add example

--- pool_grpc_test.go
+++ pool_grpc_test.go
@@ -1,6 +1,7 @@
 package pool
 
 import (
+	"log"
 	"reflect"
 	"sync"
 	"testing"
@@ -71,4 +72,47 @@ func TestGRPCPool_Get(t *testing.T) {
 			}
 		})
 	}
+}
+
+func ExampleGRPCPool() {
+	options := &Options{
+		InitTargets:  []string{"127.0.0.1:8080"},
+		InitCap:      5,
+		MaxCap:       30,
+		DialTimeout:  time.Second * 5,
+		IdleTimeout:  time.Second * 60,
+		ReadTimeout:  time.Second * 5,
+		WriteTimeout: time.Second * 5,
+	}
+
+	p, err := NewGRPCPool(options, grpc.WithInsecure())
+
+	if err != nil {
+		log.Printf("%#v\n", err)
+		return
+	}
+
+	if p == nil {
+		log.Printf("p= %#v\n", p)
+		return
+	}
+
+	defer p.Close()
+
+	//todo
+	//danamic update targets
+	//options.Input()<-&[]string{}
+
+	conn, err := p.Get()
+	if err != nil {
+		log.Printf("%#v\n", err)
+		return
+	}
+
+	defer p.Put(conn)
+
+	//todo
+	//conn.DoSomething()
+
+	log.Printf("len=%d\n", p.IdleCount())
 }
--- pool_rpc_test.go
+++ pool_rpc_test.go
@@ -1,6 +1,7 @@
 package pool
 
 import (
+	"log"
 	"net/rpc"
 	"reflect"
 	"sync"
@@ -69,4 +70,47 @@ func TestRPCPool_Get(t *testing.T) {
 			}
 		})
 	}
+}
+
+func ExampleRPCPool() {
+	options := &Options{
+		InitTargets:  []string{"127.0.0.1:8080"},
+		InitCap:      5,
+		MaxCap:       30,
+		DialTimeout:  time.Second * 5,
+		IdleTimeout:  time.Second * 60,
+		ReadTimeout:  time.Second * 5,
+		WriteTimeout: time.Second * 5,
+	}
+
+	p, err := NewRPCPool(options)
+
+	if err != nil {
+		log.Printf("%#v\n", err)
+		return
+	}
+
+	if p == nil {
+		log.Printf("p= %#v\n", p)
+		return
+	}
+
+	defer p.Close()
+
+	//todo
+	//danamic update targets
+	//options.Input()<-&[]string{}
+
+	conn, err := p.Get()
+	if err != nil {
+		log.Printf("%#v\n", err)
+		return
+	}
+
+	defer p.Put(conn)
+
+	//todo
+	//conn.DoSomething()
+
+	log.Printf("len=%d\n", p.IdleCount())
 }
